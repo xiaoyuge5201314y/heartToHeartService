76cade85906d49c1d5975ec0a17415367364d007
wudongyu <949075461@qq.com>
2022-08-26 10:54:10 +0800

    ok

---
+++ src/main/java/com/yu/hearttoheart/hearttoheart/HeartToHeartApplication.java
@@ -0,0 +1,13 @@
+package com.yu.hearttoheart.hearttoheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartToHeartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartToHeartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yu/hearttoheart/hearttoheart/HeartToHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.hearttoheart.hearttoheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartToHeartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
